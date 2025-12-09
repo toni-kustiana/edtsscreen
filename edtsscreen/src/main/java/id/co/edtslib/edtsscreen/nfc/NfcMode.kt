fd9926d95821c47f3893c1e9004b1ee308eaae34
R. Fadhil <ridhanfadhilah@gmail.com>
2025-12-09 17:23:52 +0700

    ENHANCE: use default mode

--- edtsscreen/src/main/java/id/co/edtslib/edtsscreen/nfc/NfcMode.kt
+++ edtsscreen/src/main/java/id/co/edtslib/edtsscreen/nfc/NfcMode.kt
@@ -4,20 +4,20 @@ enum class NfcMode {
     READ,
     WRITE;
 
-    fun isRead(): Boolean{
-        return this == READ;
+    fun isRead(): Boolean {
+        return this == READ
     }
 
-    fun isWrite(): Boolean{
-        return this == WRITE;
+    fun isWrite(): Boolean {
+        return this == WRITE
     }
 
-    companion object{
-        fun fromString(mode: String?): NfcMode{
-            return when(mode){
-                "READ" -> READ;
-                "WRITE" -> WRITE;
-                else -> throw IllegalArgumentException("Invalid mode: $mode");
+    companion object {
+        fun fromString(mode: String?, default: NfcMode = READ): NfcMode {
+            return when (mode) {
+                "READ" -> READ
+                "WRITE" -> WRITE
+                else -> default
             }
         }
     }
